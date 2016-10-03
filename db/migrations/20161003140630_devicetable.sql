ec6aea4adee2d570d1ed3022778976a85ab85be0
Joshua Feiber <jfeiber@Fangio.local>
2016-10-03 17:12:23 -0400

    Added a couple sample methods that use gorm and the go templating engine.

--- db/migrations/20161003140630_devicetable.sql
+++ db/migrations/20161003140630_devicetable.sql
@@ -2,13 +2,12 @@
 CREATE TABLE devices (
   id serial PRIMARY KEY,
   customer_id int NOT NULL,
-  device_name VARCHAR(255) NOT NULL,
+  device_name VARCHAR(255) UNIQUE NOT NULL,
   last_heartbeat timestamp,
   is_active boolean NOT NULL,
   party_name VARCHAR(50),
   party_size int,
-  wait_time time,
-  additional_params json
+  wait_time int DEFAULT 0
 );
 
 -- +goose Down
--- server.go
+++ server.go
@@ -5,10 +5,52 @@ import (
     "log"
     "net/http"
     "os"
+    "strconv"
     "html/template"
     "github.com/gorilla/mux"
     "github.com/urfave/negroni"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/postgres"
   )
+
+var db *gorm.DB
+
+func CreateDeviceHandler(w http.ResponseWriter, r *http.Request) {
+  log.SetPrefix("[CreateDeviceHandler] ")
+  vars := mux.Vars(r)
+  CustomerID, _ := strconv.Atoi(vars["customer_id"])
+  PartySize, _ := strconv.Atoi(vars["party_size"])
+  device := Device{CustomerID: CustomerID, DeviceName: vars["device_name"], IsActive: false, PartySize: PartySize}
+  db.NewRecord(device)
+  db.Create(&device)
+  fmt.Fprintln(w, "Device created!")
+}
+
+func FindDevicesHandler(w http.ResponseWriter, r *http.Request) {
+  log.SetPrefix("[DisplayDeviceHandler] ")
+  vars := mux.Vars(r)
+  CustomerID, _ := strconv.Atoi(vars["customer_id"])
+  var devices []Device
+  db.Where("customer_id = ?", CustomerID).Find(&devices)
+  for _, device := range devices {
+    log.Println("Device name: " + device.DeviceName)
+  }
+  t, err := template.ParseFiles("assets/templates/find_device.html.tmpl")
+  if err != nil{
+    log.Println(err)
+    log.Fatal("fail")
+  } else {
+    //Use an anonymous struct to pass data to the template.
+    data := struct {
+      CustomerID int
+      Devices []Device
+    }{
+      CustomerID,
+      devices,
+    }
+    t.Execute(w, data)
+  }
+}
 
 func RandomURLHandler(w http.ResponseWriter, r *http.Request) {
   log.SetPrefix("[RandomURLHandler] ")
@@ -18,7 +60,7 @@ func RandomURLHandler(w http.ResponseWriter, r *http.Request) {
   t, err := template.ParseFiles("assets/templates/index.html.tmpl")
   if err != nil{
     //deal with 500s later
-    log.Fatal("this is a problem")
+    log.Println("this is a problem")
     log.Fatal(err)
   } else {
     t.Execute(w, map[string] string {"Name": name})
@@ -33,7 +75,6 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
   log.SetPrefix("[main] ")
-  log.Println("hallo")
 
   //Read the server port as an ENV variable
   var port string
@@ -42,10 +83,26 @@ func main() {
   }
   log.Println("Using port: " + port)
 
+  var database_url string
+  if database_url = os.Getenv("DATABASE_URL"); database_url == ""{
+    database_url = "host="+os.Getenv("POSTGRES_PORT_5432_TCP_ADDR")+" user=ra dbname=ra password=password sslmode=disable"
+  }
+  log.Println("Using DB URL: " + database_url)
+
+  var err error
+  db, err = gorm.Open("postgres", database_url)
+  defer db.Close()
+  if err != nil{
+    log.Fatal(err)
+    panic("error connecting to db")
+  }
+
   //Setup the routes
   router := mux.NewRouter()
   router.HandleFunc("/", RootHandler)
   router.HandleFunc("/test/{name}", RandomURLHandler)
+  router.HandleFunc("/create_device/{customer_id}/{device_name}/{party_size}", CreateDeviceHandler)
+  router.HandleFunc("/find_devices/{customer_id}", FindDevicesHandler)
 
   //Tell the router to server the assets folder as static files
   router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
