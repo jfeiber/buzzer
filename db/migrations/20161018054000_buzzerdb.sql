31a0b1b1ba9a2a1c96b94ee5374cfaeca917d276
Mickey Alperin <mjalperin@gmail.com>
2016-10-18 17:25:10 -0400

    updated db model and migration to fix buzzer/active user relationship

---
+++ db/migrations/20161018054000_buzzerdb.sql
@@ -0,0 +1,54 @@
+-- +goose Up
+CREATE TABLE Restaurants (
+  id serial PRIMARY KEY,
+  name VARCHAR(99) UNIQUE NOT NULL,
+  date_created timestamp without time zone DEFAULT current_timestamp
+);
+
+CREATE TABLE Buzzers (
+  id serial PRIMARY KEY,
+  restaurant_id int REFERENCES Restaurants(id),
+  buzzer_name VARCHAR(45) NOT NULL,
+  last_heartbeat timestamp,
+  is_active boolean NOT NULL
+);
+
+CREATE TABLE ActiveParties (
+  id serial PRIMARY KEY,
+  restaurant_id int REFERENCES Restaurants(id),
+  party_name VARCHAR(50) NOT NULL,
+  party_size int NOT NULL,
+  time_created timestamp NOT NULL,
+  time_seated timestamp,
+  phone_ahead boolean NOT NULL,
+  wait_time_expected int,
+  wait_time_calculated int,
+  buzzers_id int REFERENCES Buzzers(id)
+);
+
+CREATE TABLE HistoricalParties (
+  id serial PRIMARY KEY,
+  restaurant_id int REFERENCES Restaurants(id),
+  party_name VARCHAR(50) NOT NULL,
+  party_size int NOT NULL,
+  date_created timestamp NOT NULL,
+  date_seated timestamp NOT NULL,
+  wait_time_expected timestamp NOT NULL,
+  wait_time_calculated timestamp NOT NULL
+);
+
+CREATE TABLE Users (
+  id serial PRIMARY KEY,
+  restaurant_id int REFERENCES Restaurants(id),
+  username VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  pass_salt VARCHAR(50) NOT NULL,
+  date_created timestamp without time zone DEFAULT current_timestamp
+);
+
+-- +goose Down
+DROP TABLE HistoricalParties;
+DROP TABLE Users;
+DROP TABLE ActiveParties;
+DROP TABLE Buzzers;
+DROP TABLE Restaurants;
