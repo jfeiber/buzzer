6e70e3eb97313a1aa2820009415e82c28e3be8ec
Kevin <themacexpert46@gmail.com>
2016-10-25 17:31:02 -0400

    Working dropdowns

--- assets/static/js/buzzer.js
+++ assets/static/js/buzzer.js
@@ -1,11 +1,16 @@
 console.log("sup");
 
 $(document).ready(function() {
-	$('.dropdown-menu').on('click', 'a', function(){
+	// $('.dropdown-menu').on('click', 'a', function(){
 
-	  console.log("hit callback");
-	  $(this).parents(".btn-group").find('.selection').text($(this).text());
-	  $(this).parents(".btn-group").find('.selection').val($(this).text());
+	//   console.log("hit callback");
+	//   $(this).parents(".dropdown").find('.selection').text($(this).text());
+	//   $(this).parents(".dropdown").find('.selection').val($(this).text());
 
-	});
+	// });
+
+	$(".dropdown-menu li a").click(function(){
+ 	$(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+ 	$(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+ 	});
 });
