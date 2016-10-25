a3517ca381c119af3e9b07713bf5541746337813
Kevin <themacexpert46@gmail.com>
2016-10-25 17:48:23 -0400

    Dropdowns completed

--- assets/static/js/buzzer.js
+++ assets/static/js/buzzer.js
@@ -9,8 +9,9 @@ $(document).ready(function() {
 
 	// });
 
-	$(".dropdown-menu li a").click(function(){
- 	$(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
- 	$(this).parents(".dropdown").find('.btn').val($(this).data('value'));
- 	});
+        $(".dropdown-menu li a").click(function(){
+                console.log("in handler");
+                $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+                $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+        });
 });
