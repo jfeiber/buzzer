a71c6a0b8b7229a615fac0acafe07cf50b940daf
Josh Feiber <joshua@feiber.com>
2016-10-26 18:26:49 -0400

    API method to add a new active party. Some of the frontend.

--- assets/static/js/buzzer.js
+++ assets/static/js/buzzer.js
@@ -9,9 +9,33 @@ $(document).ready(function() {
 
 	// });
 
-        $(".dropdown-menu li a").click(function(){
-                console.log("in handler");
-                $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-                $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-        });
+  $(".add-party-button").click(function(){
+    // console.log("add party button handler.");
+    partyName = $('#party-name-field').val();
+    partySize = $('.btn#party-dropdown').val();
+    waitHours = $('.btn#hours-dropdown').val();
+    waitMins = $('.btn#minutes-dropdown').val();
+    phoneAhead = $('.phone-ahead-toggle .active input').attr('id') === "phone" ? true : false;
+    if (partyName === "") {
+      alert("Missing Party Name");
+      return;
+    } else if (partySize === "") {
+      alert("Missing party size");
+      return;
+    } else if (waitHours === "") {
+      alert("Missing wait hours")
+      return;
+    } else if (waitMins === "") {
+      alert("Missing wait mins");
+      return;
+    }
+    //in the future this will load this via an AJAX call. For now I am lazy.
+    location.reload(true);
+  });
+
+  $(".dropdown-menu li a").click(function(){
+    console.log("in handler");
+    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+    $(this).parents(".dropdown").find('.btn').val($(this).text());
+  });
 });
