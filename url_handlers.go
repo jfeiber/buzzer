edad25c470a4433046fccf140815d64e320ac777
joonhyongcho <joonhyongcho@gmail.com>
2017-01-11 17:47:50 -0500

    Add new function to retrieve historical parties

--- url_handlers.go
+++ url_handlers.go
@@ -634,6 +634,28 @@ func AddUserHandler(w http.ResponseWriter, r *http.Request) {
   }
 }
 
+func returnHistoricalPartiesFromRestaurantAndTime(restaurantName string, timeCreatedText string, timeSeatedText string) []HistoricalParty {
+
+    // clean up restaurant name
+    var currRestaurant Restaurant
+    db.First(&currRestaurant, "name = ?", restaurantName)
+
+    if currRestaurant == (Restaurant{}) {
+      returnObj["status"] = "failure"
+      returnObj["error_message"] = "Party with the provided ID not found"
+    }
+
+    var historicalParties []HistoricalParty
+    var format = "asdasdad"
+
+    var timeCreated, err := time.Parse(format, timeCreatedText)
+    var timeSeated, err := time.Parse(restaurantName, timeSeatedText)
+
+    var timeCreatedFormatted := timeCreated.Format("2006-01-02 15:04:05")
+
+    db.Where("restaurant_id = ? AND time_created >= ? AND time_seated <= ?", currRestaurant.id, timeCreated, timeSeated).find(&historicalParties)
+}
+
 func IsPartyAssignedBuzzerHandler(w http.ResponseWriter, r *http.Request) {
   returnObj := map[string] interface{} {"status": "success"}
   if !IsUserLoggedIn(GetSession(w, r)) {
